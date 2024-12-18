aed13dc913560ef0579cbfaaa7b2162e36629701
dariia-star <golovadasha1@gmail.com>
2024-12-18 12:00:50 +0100

    Update Script-5.sql

--- Script-5.sql
+++ Script-5.sql
@@ -9,62 +9,62 @@ with fb as (
 	       sum(fabd.leads) as leads,
 	       sum(fabd.value ) as value 
 	from public.facebook_ads_basic_daily fabd 
-left join public.facebook_campaign fc 
-on fc.campaign_id = fabd.campaign_id 
-left join  public.facebook_adset fa 
-on fa.adset_id = fabd.adset_id
-where fabd.ad_date is not null 
-group by fabd.ad_date, 
-	fc.campaign_name,
-	fa.adset_name
+	left join public.facebook_campaign fc 
+	on fc.campaign_id = fabd.campaign_id 
+	left join  public.facebook_adset fa 
+	on fa.adset_id = fabd.adset_id
+	where fabd.ad_date is not null 
+	group by fabd.ad_date, 
+	         fc.campaign_name,
+	         fa.adset_name
 	),
-	fplusg as (
-select  ad_date,
-       'Facebook ads' as media_source,
-        campaign_name,
-	adset_name,
-	sum(spend ) as spend,
-	sum(impressions ) as impressions,
-	sum(reach ) as reach,
-	sum(clicks )as clicks,
-	sum(leads) as leads,
-	sum(value ) as value 
-from fb f 
-group by ad_date,
-	 media_source,
-	 campaign_name,
-	 adset_name 
-union all 
-select gabd.ad_date,
-       'Google ads' as media_source,
-	gabd.campaign_name,
-	gabd.adset_name,
-	sum(gabd.spend ) as spend,
-	sum(gabd.impressions ) as impressions,
-	sum(gabd.reach ) as reach,
-	sum(gabd.clicks )as clicks,
-	sum(gabd.leads) as leads,
-	sum(gabd.value ) as value 
-from public.google_ads_basic_daily gabd 
-group by ad_date,
-	 media_source,
-	 campaign_name,
-	 adset_name
+fplusg as (
+	select ad_date,
+               'Facebook ads' as media_source,
+                campaign_name,
+	        adset_name,
+	        sum(spend ) as spend,
+	        sum(impressions ) as impressions,
+	        sum(reach ) as reach,
+	        sum(clicks )as clicks,
+	        sum(leads) as leads,
+	       sum(value ) as value 
+	from fb f 
+	group by ad_date,
+	         media_source,
+	         campaign_name,
+	         adset_name 
+	union all 
+	select gabd.ad_date,
+               'Google ads' as media_source,
+	        gabd.campaign_name,
+	        gabd.adset_name,
+	        sum(gabd.spend ) as spend,
+	        sum(gabd.impressions ) as impressions,
+	        sum(gabd.reach ) as reach,
+	        sum(gabd.clicks )as clicks,
+	        sum(gabd.leads) as leads,
+	        sum(gabd.value ) as value 
+	from public.google_ads_basic_daily gabd 
+	group by ad_date,
+	         media_source,
+	         campaign_name,
+	         adset_name
 ),
 total as ( 
-select ad_date,
-       media_source,
-       campaign_name,
-       adset_name,
-       sum(spend) as spend,
-       sum(impressions) as impressions,
-       sum(clicks) as clicks,
-       sum(value) as value
-from fplusg
-group by ad_date,
-	 media_source,
-	 campaign_name,
-	 adset_name 
+	select ad_date,
+               media_source,
+               campaign_name,
+               adset_name,
+               sum(spend) as spend,
+               sum(impressions) as impressions,
+               sum(clicks) as clicks,
+               sum(value) as value
+	from fplusg
+	group by ad_date,
+	         media_source,
+	         campaign_name,
+	         adset_name 
 )
 select media_source,
        campaign_name,
