e1418b8f4612edb6538d5b2721d672e5608a4f41
dariia-star <golovadasha1@gmail.com>
2024-12-18 11:37:23 +0100

    Update Script-3.sql
    Fix

--- Script-3.sql
+++ Script-3.sql
@@ -1,11 +1,8 @@
-
-
-select 
-	campaign_id,
+select  campaign_id,
 	sum(spend) as spend, 
 	(sum(value :: numeric)- sum(spend :: numeric))/ sum(spend) as romi
 from public.facebook_ads_basic_daily fabd 
 group by fabd.campaign_id 
 having sum(spend)> 500000
 order by romi desc 
-limit 1 ;
+limit 1;
