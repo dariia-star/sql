c7e13ffdbcefc7783c1476543594252bc32b853b
dariia-star <golovadasha1@gmail.com>
2024-12-18 11:40:24 +0100

    Update Script-4.sql

--- Script-4.sql
+++ Script-4.sql
@@ -1,17 +1,18 @@
-
-
-with cte_table  as (select fabd.ad_date , fabd.spend , fabd.impressions , fabd.reach , 
-fabd.clicks , fabd.leads , fabd.value , 'facebook' as media_source 
-from public.facebook_ads_basic_daily fabd
-union all
-select gabd.ad_date , gabd.spend , gabd.impressions , gabd.reach ,
-gabd.clicks , gabd.leads , gabd.value , 'google' as media_source 
-from public.google_ads_basic_daily gabd) 
-select  ad_date, media_source,
-sum(spend) as spend ,
-sum(impressions) as impressions ,
-sum(clicks) as clicks,
-sum(value) as value 
+with cte_table  as (
+    select fabd.ad_date, fabd.spend, fabd.impressions, fabd.reach, 
+    fabd.clicks, fabd.leads, fabd.value, 'facebook' as media_source 
+    from public.facebook_ads_basic_daily fabd
+    union all
+    select gabd.ad_date, gabd.spend, gabd.impressions, gabd.reach,
+    gabd.clicks, gabd.leads, gabd.value, 'google' as media_source 
+    from public.google_ads_basic_daily gabd
+  )
+select ad_date, 
+       media_source,
+       sum(spend) as spend,
+       sum(impressions) as impressions,
+       sum(clicks) as clicks,
+       sum(value) as value 
 from cte_table
 group by ad_date, media_source;
 
