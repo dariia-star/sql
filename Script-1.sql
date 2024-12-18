0831a83598341bd8e2f56aeea7c160540f588c8d
dariia-star <golovadasha1@gmail.com>
2024-12-18 11:35:09 +0100

    Update Script-1.sql
    Fix errors

--- Script-1.sql
+++ Script-1.sql
@@ -1,22 +1,19 @@
-select 
-	fabd.ad_date,
-	fabd.spend ,
+select	fabd.ad_date,
+	fabd.spend,
 	fabd.clicks
-from public.facebook_ads_basic_daily fabd ;
+from public.facebook_ads_basic_daily fabd;
 
-select 
-	fabd.ad_date,
-	fabd.spend ,
+select 	fabd.ad_date,
+	fabd.spend,
 	fabd.clicks,
 	fabd.spend / fabd.clicks 
 from public.facebook_ads_basic_daily fabd 
 where fabd.clicks >0;
 
-select 
-	fabd.ad_date,
-	fabd.spend ,
+select 	fabd.ad_date,
+	fabd.spend,
 	fabd.clicks,
 	fabd.spend / fabd.clicks as S2C
 from public.facebook_ads_basic_daily fabd 
 where fabd.clicks >0
-order by fabd.ad_date desc ;
+order by fabd.ad_date desc;
