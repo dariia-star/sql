34f40419ee35775519eca357ba2249f9a4d3213a
dariia-star <golovadasha1@gmail.com>
2024-12-18 12:04:47 +0100

    Update Script-7.sql

--- Script-7.sql
+++ Script-7.sql
@@ -1,38 +1,41 @@
-with fplusg as (select fabd.ad_date ,
-	   fabd.url_parameters ,
-	   fabd.spend ,
-	   fabd.impressions , 
-	   fabd.reach , 
-	   fabd.clicks ,
-	   fabd.leads ,
-	   fabd.value 
-from public.facebook_ads_basic_daily fabd 
-left join public.facebook_campaign fc 
-	on fabd.campaign_id = fc.campaign_id 
-left join public.facebook_adset fa 
-	on fabd.adset_id = fa.adset_id 
-union all 
-select gabd.ad_date,
-       gabd.url_parameters ,
-       gabd.spend ,
-	   gabd.impressions , 
-	   gabd.reach , 
-	   gabd.clicks ,
-	   gabd.leads ,
-	   gabd.value 
-from public.google_ads_basic_daily gabd
+with fplusg as (
+	select fabd.ad_date,
+	       fabd.url_parameters,
+	       fabd.spend,
+	       fabd.impressions, 
+	       fabd.reach, 
+	       fabd.clicks,
+	       fabd.leads,
+	       fabd.value 
+	from public.facebook_ads_basic_daily fabd 
+	left join public.facebook_campaign fc 
+		on fabd.campaign_id = fc.campaign_id 
+	left join public.facebook_adset fa 
+		on fabd.adset_id = fa.adset_id 
+	union all 
+	select gabd.ad_date,
+               gabd.url_parameters,
+               gabd.spend,
+	       gabd.impressions, 
+	       gabd.reach, 
+	       gabd.clicks,
+	       gabd.leads,
+	       gabd.value 
+	from public.google_ads_basic_daily gabd
 ),
-total_fg as (select ad_date,
-       case when lower(substring(url_parameters,'utm_campaign=([^\&]+)'))='nan' 
-       then null else lower(substring(url_parameters,'utm_campaign=([^\&]+)')) 
-       end as utm_campaign,
-       coalesce (spend,0) as spend ,
-	   coalesce (impressions,0) as impressions , 
-	   coalesce (reach,0) as reach  , 
-	   coalesce (clicks,0) as clicks ,
-	   coalesce (leads,0) as leads ,
-	   coalesce (value,0) as value
-from fplusg )
+total_fg as (
+	select ad_date,
+        case when lower(substring(url_parameters,'utm_campaign=([^\&]+)'))='nan' 
+        then null else lower(substring(url_parameters,'utm_campaign=([^\&]+)')) 
+        end as utm_campaign,
+               coalesce (spend,0) as spend,
+	       coalesce (impressions,0) as impressions, 
+	       coalesce (reach,0) as reach, 
+	       coalesce (clicks,0) as clicks,
+	       coalesce (leads,0) as leads,
+	       coalesce (value,0) as value
+	from fplusg 
+)
 select ad_date,
        utm_campaign,
        sum(spend ) as total_spend,
